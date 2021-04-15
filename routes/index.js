173e99247349e94115dce1d6239daafe94b3f148
i'mfebri <iamfebriadji@gmail.com>
2021-04-15 23:14:25 +0700

    menambahkan routing dengan method post untuk todopost

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const TodoControllers = require('../controllers/todo')
 const controllers = new TodoControllers()
 
 router.get('/todo', controllers.todoGet)
+router.post('/todo', controllers.todoPost)
 
 module.exports = router
