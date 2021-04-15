46c3f5f417bb5939d4c32c7df938848f1e9f229d
i'mfebri <iamfebriadji@gmail.com>
2021-04-15 22:45:06 +0700

    inisialisasi routes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const router = require('express').Router()
+
+const TodoControllers = require('../controllers/todo')
+const controllers = new TodoControllers()
+
+router.get('/todo', controllers.todoGet)
+
+module.exports = router
