0ba71531f01f2180ec25a09c796b1bc3289f3742
i'mfebri <iamfebriadji@gmail.com>
2021-04-16 11:26:47 +0700

    inisialisasi komponen tablelist

---
+++ client/src/components/TableList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TableList({ result }) {
+  return (
+    <div className="list">
+      <h2 className="title">{ result.title }</h2>
+    </div>
+  )
+}
+
+export default TableList
