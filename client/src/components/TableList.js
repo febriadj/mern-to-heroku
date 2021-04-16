8261afe2e254288786ddaea1f82131bd947a3edc
i'mfebri <iamfebriadji@gmail.com>
2021-04-16 15:07:00 +0700

    menambahkan waktu todo dibuat dan button delete

--- client/src/components/TableList.js
+++ client/src/components/TableList.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
-function TableList({ result }) {
+function TableList({ result, deleteTodo }) {
+  function created() {
+    const date = new Date(result.createdAt)
+    return date.toLocaleDateString('id-ID', {
+      weekday: 'short', day: '2-digit', month: 'short', year: 'numeric',  // tanggal bulan dan tahun
+      hour: '2-digit', minute: '2-digit',                                 // jam dan menit
+      timeZone: 'Asia/Jakarta', timeZoneName: 'short'                     // zona waktu
+    })
+  }
+
   return (
     <div className="list">
-      <h2 className="title">{ result.title }</h2>
+      <div className="list-info">
+        <h2 className="title">{ result.title }</h2>
+        <p className="created-at">{ created() }</p>
+      </div>
+      <div className="list-action">
+        <button
+          className="fas fa-trash"
+          value={ result._id }
+          onClick={ deleteTodo }
+        >
+        </button>
+      </div>
     </div>
   )
 }
