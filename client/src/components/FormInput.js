4be6be65f0a140631c8a626afd47d499b9c915c7
i'mfebri <iamfebriadji@gmail.com>
2021-04-16 16:49:38 +0700

    menambahkan paragraf dan link github

--- client/src/components/FormInput.js
+++ client/src/components/FormInput.js
@@ -3,13 +3,22 @@ import React from 'react'
 function FormInput({ inputChange, inputSubmit, valueForm }) {
   return (
     <form className="form-input" method="post" onSubmit={ inputSubmit }>
-      <input 
-        name="title" 
-        placeholder="Apa Judul Kegiatannya?"
-        onChange={ inputChange }
-        value={ valueForm.title }
-      />
-      <button type="submit">Tambah</button>
+      <div className="input-action">
+        <input 
+          name="title" 
+          placeholder="Apa Judul Kegiatannya?"
+          onChange={ inputChange }
+          value={ valueForm.title }
+        />
+        <button 
+          className="fas fa-arrow-right"
+          type="submit"
+        >
+        </button>
+      </div>
+      <p className="github">
+        Kunjungi <a href="https://github.com/febriadj/mern-to-heroku">GitHub</a>, Untuk Melihat Kode Program
+      </p>
     </form>
   )
 }
